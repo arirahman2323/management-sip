d45b06acaf993f5c8c2078e021a6babe60a596ca
hfz <redptr13@gmail.com>
2025-07-15 20:05:23 +0700

    Add LoginRegister

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
