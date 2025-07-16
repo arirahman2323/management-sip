ef8927dd80d391111418832400755979921155d6
hfz <redptr13@gmail.com>
2025-07-16 19:09:09 +0700

    Add Login and Register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
 
 const App = () => {
-  return <div className="">App</div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ frontend/src/pages/Auth/Login.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { Link } from "react-router-dom";
+import LogoAuth from "../../components/logo/BlueLogo";
+
+const Login = () => {
+  return (
+    <AuthLayout imageSide="right">
+      <div className="w-full max-w-md space-y-8">
+        <div>
+          <LogoAuth className="justify-left" />
+          <h2 className="text-center text-3xl font-bold tracking-tight text-gray-900">
+            Masuk Aplikasi
+          </h2>
+          <p className="mt-2 text-center text-sm text-gray-600">
+            Masukkan Email dan Password untuk masuk.
+          </p>
+        </div>
+
+        <form className="mt-8 space-y-6" action="#" method="POST">
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="email-address" className="sr-only">
+                Email
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                required
+                className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Email"
+              />
+            </div>
+            <div className="pt-4">
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+              />
+              <label
+                htmlFor="remember-me"
+                className="ml-2 block text-sm text-gray-900"
+              >
+                Ingatkan saya
+              </label>
+            </div>
+            <div className="text-sm">
+              <a
+                href="#"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Lupa Password
+              </a>
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Masuk
+            </button>
+          </div>
+        </form>
+        <p className="mt-2 text-center text-sm text-gray-600">
+          Tidak Punya Akun?
+          <Link
+            to="/register"
+            className="ml-1 font-medium text-indigo-600 hover:text-indigo-500"
+          >
+            Klik disini untuk mendaftar.
+          </Link>
+        </p>
+      </div>
+    </AuthLayout>
+  );
+};
+
+export default Login;
