64b5d98a9c6845cf3975d73c47d02b14e8c8eaee
hfz <redptr13@gmail.com>
2025-07-28 18:21:10 +0700

    Fix Button in ProductWillExp and ProductExp

--- frontend/src/pages/Admin/ProductExpired/Exp.jsx
+++ frontend/src/pages/Admin/ProductExpired/Exp.jsx
@@ -7,7 +7,7 @@ const Exp = ({ isOpen, onClose }) => {
   return (
     <div className="fixed inset-0 bg-transparent bg-opacity-50 backdrop-blur-sm flex justify-center items-center z-50">
       <div className="bg-white rounded-lg shadow-xl p-6 w-full max-w-lg">
-        <div className="flex justify-between items-center border-b pb-3 mb-4">
+        <div className="flex justify-between items-center border-b border-gray-100 pb-3 mb-4">
           <h2 className="text-xl font-bold text-gray-800">
             Form Restock Barang
           </h2>
@@ -20,33 +20,31 @@ const Exp = ({ isOpen, onClose }) => {
         </div>
         {/* Form Product */}
         <form className="space-y-4">
-          <div>
-            <label
-              htmlFor="jumlah"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Jumlah Restock Barang
-            </label>
+          {/* Jumlah Barang */}
+          <div className="flex flex-col">
+            <label className="text-sm font-normal">Jumlah Restock Barang</label>
             <input
               type="number"
-              id="jumlah"
-              placeholder="50"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
+              name="number"
+              value="{formData.name}"
+              onChange="{onChange}"
+              className="bg-gray-50 text-gray-500 text-sm p-3 rounded-lg focus:outline-gray-500"
+              placeholder="Jumlah"
             />
           </div>
         </form>
 
-        <div className="mt-6 flex justify-end gap-4 border-t pt-4">
+        <div className="flex justify-between gap-4 mt-4">
           <button
-            onClick={onClose}
             type="button"
-            className="bg-white border border-gray-300 text-gray-800 px-6 py-2 rounded-lg hover:bg-gray-50"
+            onClick={onClose}
+            className="px-4 w-full py-2 border rounded-md hover:bg-gray-100 cursor-pointer"
           >
             Cancel
           </button>
           <button
             type="submit"
-            className="bg-blue-600 text-white px-8 py-2 rounded-lg hover:bg-blue-700"
+            className="px-4 py-2 w-full bg-indigo-500 text-white rounded-md hover:bg-indigo-600 cursor-pointer"
           >
             Save
           </button>
--- frontend/src/pages/Admin/ProductWillExpired/Index.jsx
+++ frontend/src/pages/Admin/ProductWillExpired/Index.jsx
@@ -5,27 +5,56 @@ import SearchInput from "../../../components/layouts/ProductSearch";
 import { products as initialProducts } from "../../../data/productData";
 import { columns as defineColumns } from "./Column";
 import WillExp from "./WillExp";
+import axiosInstance from "../../../utils/axiosInstance";
+import { API_PATHS } from "../../../utils/apiPath";
+import { FaFilter } from "react-icons/fa";
 
 const Index = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [data, setData] = useState(initialProducts);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState(null);
+  const [searchField, setSearchField] = useState("all");
+  const [filteredData, setFilteredData] = useState([]);
 
   const handleEdit = (row) => {
-    setSelectedProduct(row); // Simpan data baris yang dipilih
-    setIsModalOpen(true); // Buka modal
+    setSelectedProduct(row);
+    setIsModalOpen(true);
   };
 
   const columns = defineColumns(handleEdit);
 
-  // Searching Produk
+  const fetchProducts = async () => {
+    try {
+      const res = await axiosInstance.get(API_PATHS.PRODUCT.GET_PRODUCT);
+      setData(res.data.data);
+      setFilteredData(res.data.data);
+    } catch (err) {
+      console.error("Error fetching products:", err);
+    }
+  };
+
   useEffect(() => {
-    const result = initialProducts.filter((product) =>
-      product.name.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-    setData(result);
-  }, [searchQuery]);
+    fetchProducts();
+  }, []);
+  // Pencarian
+  useEffect(() => {
+    const result = data.filter((product) => {
+      if (searchField === "all") {
+        return Object.values(product).some((val) =>
+          val?.toString().toLowerCase().includes(searchQuery.toLowerCase())
+        );
+      } else {
+        const fieldValue = product[searchField];
+        return fieldValue
+          ?.toString()
+          .toLowerCase()
+          .includes(searchQuery.toLowerCase());
+      }
+    });
+
+    setFilteredData(result);
+  }, [searchQuery, data]);
 
   return (
     <div>
@@ -41,17 +70,51 @@ const Index = () => {
             <div className="flex flex-col bg-white p-4 rounded-lg shadow-md">
               <div className="flex justify-between items-center mb-4">
                 {/*Pencarian */}
-                <SearchInput
-                  value={searchQuery}
-                  onChange={(e) => setSearchQuery(e.target.value)}
-                  placeholder="Mencari Barang berdasarkan Nama..."
-                />
+                <div className="flex items-center border-2 border-gray-300 rounded-lg overflow-hidden w-full max-w-md">
+                  <FaFilter size={20} className="ml-2 text-gray-500" />
+                  <select
+                    value={searchField}
+                    onChange={(e) => setSearchField(e.target.value)}
+                    className="p-2 text-sm border-r-2 border-gray-300 outline-none text-gray-700"
+                  >
+                    <option value="all">All</option>
+                    <option value="sku">Kode Barang</option>
+                    <option value="name">Nama</option>
+                    <option value="item_name">Jenis Barang</option>
+                    <option value="unit_name">Satuan</option>
+                    <option value="price">Harga Beli</option>
+                    <option value="price_sell">Harga Jual</option>
+                  </select>
+                  <div className="flex items-center px-2 text-gray-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-4 w-4"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                      />
+                    </svg>
+                  </div>
+                  <input
+                    type="text"
+                    className="w-full p-2 text-sm outline-none"
+                    placeholder="Search"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                  />
+                </div>
               </div>
 
               {/* Data Table Produk */}
               <DataTable
                 columns={columns}
-                data={data}
+                data={filteredData}
                 pagination
                 highlightOnHover
               />
